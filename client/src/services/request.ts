90519af67e3db37a946be90daa1838b684beb1b6
Victoria Fonteny <victoriafonteny@live.fr>
2025-02-03 10:11:14 +0100

    step1

---
+++ client/src/services/request.ts
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+const getAllCupcake = () => {
+  return axios
+    .get("http://localhost:3310/api/cupcakes")
+    .then((response) => response.data)
+    .catch((error) => console.error(error));
+};
+
+export { getAllCupcake };
