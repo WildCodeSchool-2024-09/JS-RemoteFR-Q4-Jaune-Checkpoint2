d342571b106e574d9600fa93a5db34681a83409a
Jon33260 <j.mallet82@laposte.net>
2025-02-03 12:07:00 +0100

    step2 finish

--- client/src/pages/CupcakeList.tsx
+++ client/src/pages/CupcakeList.tsx
@@ -62,6 +62,9 @@ function CupcakeList() {
       </form>
       <ul className="cupcake-list" id="cupcake-list">
         {/* Step 2: repeat this block for each cupcake */}
+        {CupcakeList.map((cupcake) => (
+          <Cupcake key={cupcake.id} data={cupcake} />
+        ))}
         {/* Step 5: filter cupcakes before repeating */}
         <li className="cupcake-item">
           <Cupcake data={sampleCupcakes[0]} />
