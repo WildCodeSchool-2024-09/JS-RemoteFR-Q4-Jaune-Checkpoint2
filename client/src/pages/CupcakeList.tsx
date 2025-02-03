ec970cc739498eb60699ba9e980ed01ff96bc85b
Clarelle974 <clarelle.lemonnier@gmail.com>
2025-02-03 12:27:30 +0100

    Cupcakes STEP5

--- client/src/pages/CupcakeList.tsx
+++ client/src/pages/CupcakeList.tsx
@@ -1,41 +1,9 @@
-import { useLoaderData } from "react-router-dom";
-import Cupcake from "../components/Cupcake";
 import axios from "axios";
 import { useEffect, useState } from "react";
-
-/* ************************************************************************* */
-// const sampleCupcakes = [
-//   {
-//     id: 10,
-//     accessory_id: "4",
-//     accessory: "wcs",
-//     color1: "blue",
-//     color2: "white",
-//     color3: "red",
-//     name: "France",
-//   },
-//   {
-//     id: 11,
-//     accessory_id: "4",
-//     accessory: "wcs",
-//     color1: "yellow",
-//     color2: "red",
-//     color3: "black",
-//     name: "Germany",
-//   },
-//   {
-//     id: 27,
-//     accessory_id: "5",
-//     accessory: "christmas-candy",
-//     color1: "yellow",
-//     color2: "blue",
-//     color3: "blue",
-//     name: "Sweden",
-//   },
-// ];
+import { useLoaderData } from "react-router-dom";
+import Cupcake from "../components/Cupcake";
 
 type CupcakeArray = CupcakeData[];
-// interface CupcakeArray {[CupcakeData]};
 
 interface CupcakeData {
   id: number;
@@ -55,14 +23,12 @@ interface AccessoryData {
   };
 }
 
-/* you can use sampleCupcakes if you're stucked on step 1 */
-/* if you're fine with step 1, just ignore this ;) */
-/* ************************************************************************* */
-
 function CupcakeList() {
   // Step 1: get all cupcakes
   const data = useLoaderData() as CupcakeArray;
   const [accessoryData, setAccessoryData] = useState([] as AccessoryData[]);
+  const [selectedAccessory, setSelectedAccessory] = useState("");
+
   useEffect(() => {
     axios
       .get("http://localhost:3310/api/accessories")
@@ -71,12 +37,19 @@ function CupcakeList() {
       })
       .catch((error) => console.error(error));
   }, []);
-  // useEffect();
-  console.info(accessoryData);
+
+  const handleAccessoryChange = (
+    event: React.ChangeEvent<HTMLSelectElement>,
+  ) => {
+    setSelectedAccessory(event.target.value);
+  };
 
   // Step 3: get all accessories
 
   // Step 5: create filter state
+  const filteredData = selectedAccessory
+    ? data.filter((cupcake) => cupcake.accessory_id === selectedAccessory)
+    : data;
 
   return (
     <>
@@ -85,8 +58,19 @@ function CupcakeList() {
         <label htmlFor="cupcake-select">
           {/* Step 5: use a controlled component for select */}
           Filter by{" "}
-          <select id="cupcake-select">
+          <select
+            id="cupcake-select"
+            required
+            aria-label="sélectionner un supplément"
+            value={selectedAccessory}
+            onChange={handleAccessoryChange}
+          >
             <option value="">---</option>
+            <option value="1">Cherry</option>
+            <option value="2">Donut</option>
+            <option value="3">Chocolate</option>
+            <option value="4">Wild</option>
+            <option value="5">Christmas Candy</option>
             {/* Step 4: add an option for each accessory */}
           </select>
         </label>
@@ -95,7 +79,7 @@ function CupcakeList() {
         {/* Step 2: repeat this block for each cupcake */}
         {/* Step 5: filter cupcakes before repeating */}
         <li className="cupcake-item">
-          {data.map((cupcake) => (
+          {filteredData.map((cupcake) => (
             <Cupcake key={cupcake.id} data={cupcake} />
           ))}
         </li>
