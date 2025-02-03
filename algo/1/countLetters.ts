8f00fa0b58619465ca8dfccf736cee59c0558ef5
Ochoooooo <greenfieldrp7@gmail.com>
2025-02-03 09:46:42 +0100

    Set 1 algo 1

--- algo/1/countLetters.ts
+++ algo/1/countLetters.ts
@@ -10,7 +10,10 @@ Exemples :
 */
 
 function countLetters(givenString: string, letter: string): number {
-  // Your code here !
+  if (givenString.includes(letter)) {
+    return givenString.split(letter).length - 1;
+  }
+
   return 0;
 }
 
