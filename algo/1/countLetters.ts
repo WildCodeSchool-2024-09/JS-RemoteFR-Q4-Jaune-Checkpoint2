3eea0d8ef14da58017a8c78d7c026efbf7f42619
maxime <trousselmaxime@gmail.com>
2025-02-03 10:02:32 +0100

    algo 1

--- algo/1/countLetters.ts
+++ algo/1/countLetters.ts
@@ -11,7 +11,14 @@ Exemples :
 
 function countLetters(givenString: string, letter: string): number {
   // Your code here !
-  return 0;
+  let count = 0;
+
+  for (let i = 0; i < givenString.length; i++)
+    if (givenString[i] === letter) {
+      count++; // Incrémentation du compteur
+    }
+
+  return count;
 }
 
 export default countLetters;
